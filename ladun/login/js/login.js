49b8ed9d44de2dcf6df36d45d121df5590d5764c
haxorsprogramming <haxorsuinsu@gmail.com>
2020-02-13 02:33:44 +0700

    selamanya

--- ladun/login/js/login.js
+++ ladun/login/js/login.js
@@ -1,34 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $("#txtUsername").focus();
 
-  $('#txtUsername').focus();
+  $("#btnMasuk").click(function() {
+    let username = $("#txtUsername").val();
+    let password = $("#txtPassword").val();
+    let data_send = { username: username, password: password };
 
-  $('#btnMasuk').click(function(){
-    let username = $('#txtUsername').val();
-    let password = $('#txtPassword').val();
-    let data_send = {'username':username, 'password':password}
-
-    if(username == '' || password == ''){
-      pesan('Isi field!!','Harap isi username & password','warning','Ok');
-    }else{
-      $.post('api/login_proses.php',{'username':username, 'password':password},function(data){
-        let obj = JSON.parse(data);
-        let status = obj.status;
-        console.log(obj);
-        if(status == 'success'){
-         window.location.assign('mainApp/');
-        }else{
-          pesan('Login gagal','Username / Password salah, periksa kembali','error','Ok');
-          $('#txtUsername').focus();
-          $('#txtPassword').val("");          
+    if (username == "" || password == "") {
+      pesan("Isi field!!", "Harap isi username & password", "warning", "Ok");
+    } else {
+      $.post(
+        "api/login_proses.php",
+        { username: username, password: password },
+        function(data) {
+          let obj = JSON.parse(data);
+          let status = obj.status;
+          console.log(obj);
+          if (status == "success") {
+            window.location.assign("mainApp/");
+          } else {
+            pesan(
+              "Login gagal",
+              "Username / Password salah, periksa kembali",
+              "error",
+              "Ok"
+            );
+            $("#txtUsername").focus();
+            $("#txtPassword").val("");
+          }
         }
-      });
+      );
     }
-
-    
   });
 
-  function pesan(title, isi, icon, btn){
-    Swal.fire({title: title,text: isi,icon: icon,confirmButtonText: btn })
+  function pesan(title, isi, icon, btn) {
+    Swal.fire({ title: title, text: isi, icon: icon, confirmButtonText: btn });
   }
-
 });
